582cc36c8516ace39b92c4ee1622f15c8876c15e
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-02-18 11:11:37 +0900

    4일차 큐 트리

--- day04/da01_queue.py
+++ day04/da01_queue.py
@@ -2,12 +2,27 @@
 # 큐 자료구조 구현
 
 # 함수 선언
-def isQururFull():
+def isQueueFull():
     global SIZE, queue, front, rear
-    if rear == SIZE - 1:
+    # 1. 가장 일반적 로직
+    # if front == rear:
+    #     return True
+    # else:
+    #     return False
+    # 2. 개선 로직
+    if rear != SIZE - 1:
+        return False
+    elif rear == SIZE -1 and front == -1:
         return True
     else:
+        for i in range(front + 1, SIZE):
+            queue[i-1] = queue[i]   # 데이터앞 빈자리에 첫번째 데이터 옮김
+            queue[i] = None
+            
+        front -= 1
+        rear -= 1
         return False
+    
     
 def isQueueEmpty():
     global SIZE, queue, front, rear
@@ -15,10 +30,12 @@ def isQueueEmpty():
         return True
     else:
         return False
+    
+    
 
 def enQueue(data):
     global SIZE, queue, front, rear
-    if isQururFull():
+    if isQueueFull():
         print('Queue is full')
     else:
         rear += 1
@@ -28,7 +45,6 @@ def deQueue():
     global SIZE, queue, front, rear
     if isQueueEmpty():
         print('Queue is empty')
-        return None
     else:
         front += 1
         data = queue[front]
@@ -50,15 +66,24 @@ front = rear = -1
 
 ## 메인 모듈
 if __name__ == '__main__':
-    print('메인시작')
+    # print('메인시작')
     while True:
-        select = input('삽입(I)/추출(E)/확인(V)/종료(Q)').upper
-
+        select = input('삽입(I)/추출(E)/확인(V)/종료(Q)').upper()
         if select == 'Q':
             break
         elif select == 'I':
             data = input('데이터 입력 > ')
             enQueue(data)
             print(f'큐 상태 : {queue}')
+        elif select == 'E':
+            data = deQueue()
+            print(f'추출 데이터 : {data}')
+            print(f'큐 상태 : {queue}')
+        elif select == 'V':
+            data = peek()
+            print(f'확인 데이터 : {data}')
+            print(f'큐 상태 : {queue}')
+
         else:
             print('입력 오류')
+        
